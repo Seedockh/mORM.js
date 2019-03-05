21da6549e46c344a463bb8ea9f7ef3ec9ac4526c
efrei <efrei@B15-04.local>
2019-03-05 10:09:11 +0100

    setting env

---
+++ src/mOrm.js
@@ -0,0 +1,10 @@
+export default class mOrm {
+  configPathName = "./morm.config.json";
+
+  //if json
+  //if empty
+  //else
+  async createConnection(dbConfig = configPathName) {
+    console.log("ON CREATE");
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
-class Test {
-  
-}
+import mOrm from "./mOrm";
+
+(async () => {
+  const orm = new mOrm();
+
+  try {
+    await orm.createConnection({
+      "type": "postgres",
+      "host": "localhost",
+      "port": 5432,
+      "username": "efrei",
+      "password": "efrei",
+      "database": "iLovePragmatic"
+    });
+  } catch(err) {
+    console.log(err);
+    process.exit(-1);
+  }
+})();
